10c8223a6d40cce32977cd12c1a17f12df48bebd
Kreedciik <68077959+Kreedciik@users.noreply.github.com>
2020-07-10 01:13:09 +0500

    Irvas
    10.07.2020

---
+++ Irvas/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    
+    $('#telMask').mask("+7(999) 999-99-99")
+    $('#telMask2').mask("+7(999) 999-99-99")
+    $('#telMask3').mask("+7(999) 999-99-99")
+
+    // Timer script
+    
+
+});
